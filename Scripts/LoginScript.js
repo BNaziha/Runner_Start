1e33c33fc93eb19e36c76877c2853d477b28e13a
lemmouchimds <lemmouchimds@gmail.com>
2023-11-30 18:17:55 +0100

    add validate email and passcode to login

--- Scripts/LoginScript.js
+++ Scripts/LoginScript.js
@@ -1,17 +1,82 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-const sign_in_btn2 = document.querySelector("#sign-in-btn2");
-const sign_up_btn2 = document.querySelector("#sign-up-btn2");
-sign_up_btn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
+
+$(document).ready(function() {
+    const sign_in_btn = $("#sign-in-btn");
+    const sign_up_btn = $("#sign-up-btn");
+    const container = $(".container");
+    const sign_in_btn2 = $("#sign-in-btn2");
+    const sign_up_btn2 = $("#sign-up-btn2");
+
+    sign_up_btn.on("click", function() {
+        container.addClass("sign-up-mode");
+    });
+
+    sign_in_btn.on("click", function() {
+        container.removeClass("sign-up-mode");
+    });
+
+    sign_up_btn2.on("click", function() {
+        container.addClass("sign-up-mode2");
+    });
+
+    sign_in_btn2.on("click", function() {
+        container.removeClass("sign-up-mode2");
+    });
+
+    function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+
+    $(".email").keydown(function() {
+        let email = $(".email").val();
+        if (validateEmail(email)) {
+            $(".input-email").css("border", "2px solid rgb(129, 197, 154)");
+            
+        } else {
+            $(".input-email").css("border", "2px solid red");   
+        }
+            
+    });
+    
+    
+    function validatePasscode(passcode) {
+        var re = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/";
+        return re.test(passcode);
+    }
+
+    $(".password").keyup(function() {
+        let pass = $(".password").val();
+        if (validatePasscode(pass)) {
+            $(".input-password").css("border", "2px solid rgb(129, 197, 154)");
+            
+        } else {
+            $(".input-password").css("border", "2px solid red");   
+        }
+            
+    });
 });
-sign_in_btn.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode");
-});
-sign_up_btn2.addEventListener("click", () => {
-    container.classList.add("sign-up-mode2");
-});
-sign_in_btn2.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode2");
-});
+
+
+function validateForm() {
+    var email = document.forms["myForm"]["email"].value;
+    var passcode = document.forms["myForm"]["passcode"].value;
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    if (passcode == "") {
+        alert("Passcode must be filled out");
+        return false;
+    }
+    if (!validateEmail(email)) {
+        alert("Email is not valid");
+        return false;
+    }
+    if (!validatePasscode(passcode)) {
+        alert("Passcode is not valid");
+        return false;
+    }
+}
+
+
+
